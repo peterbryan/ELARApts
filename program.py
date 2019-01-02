d02aa3e1c3b1d7a62a808477b2833a04fd944944
Peter Bryan <peterbryanr@gmail.com>
2019-01-01 20:14:04 -0800

    Fixed bug where if a student has less than 6 courses, program would crash by wrapping attempts to store data in a try/exception block and skipping student

--- program.py
+++ program.py
@@ -5,14 +5,16 @@
 import csv
 
 
-with open('srsqtr4raw.csv', encoding='utf-8-sig') as exampleFile:
+with open('srsqtr1raw.csv', encoding='utf-8-sig') as exampleFile:
     exampleReader = csv.reader(exampleFile)
   
     exampleData= list(exampleReader)
 #print(exampleData[0])
 #print(exampleData[1])
 #print(exampleData[2])
-
+#QTR1=0       #QTR3=2
+#QTR2=1       #QTR4=3
+qtr=0
 data={}
 #####Load Data###
 #for i in range(1,1000,1):
@@ -30,45 +32,57 @@ for i in range(1,len(exampleData)-1):
         data[currentStudent]['Tpts']=0
     data[currentStudent]['class'][currentPeriod]['Class Name']=exampleData[i][27]
     data[currentStudent]['class'][currentPeriod]['Teacher Name']=exampleData[i][28]
-    data[currentStudent]['class'][currentPeriod][exampleData[i][34]]=exampleData[i][35]
+    data[currentStudent]['class'][currentPeriod][exampleData[i][34]]=exampleData[i][35+qtr]
     data[currentStudent]['class'][currentPeriod]['ABS']=exampleData[i][30]
     data[currentStudent]['class'][currentPeriod]['TAR']=exampleData[i][31]
     data[currentStudent]['class'][currentPeriod]['pts']=0
 
 ##### Work with each student now
 ##### module 1 / QTR 1 #######
+##Module still needs to account for when students do not have all 6 classes.
 print('ready')
+
 for currentStudent in data:
     print(currentStudent)
     data[currentStudent]['points']=0
     for j in range(1,7):
-        if data[currentStudent]['class'][str(j)][' ACAD'] is 'M' or 'A' or 'B' or 'C':
-            data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+3
-        elif data[currentStudent]['class'][str(j)][' ACAD'] is 'D':
-            data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+1
-        if data[currentStudent]['class'][str(j)]['COOP'] is 'E' or 'S':
-            data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+2
+        try:
+            if data[currentStudent]['class'][str(j)][' ACAD'] is 'M' or 'A' or 'B' or 'C':
+                data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+3
+            elif data[currentStudent]['class'][str(j)][' ACAD'] is 'D':
+                data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+1
+            if data[currentStudent]['class'][str(j)]['COOP'] is 'E' or 'S':
+                data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+2
+            wh =False;
+            if data[currentStudent]['class'][str(j)]['ABS'] is '0' and int(data[currentStudent]['class'][str(j)]['TAR']) < 3:
+                wh=True
+                data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+2
+            if wh is True:
+                data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+2
+           #HomeRoom Below.
+        except KeyError:
+            print('Error with '+currentStudent+' could not find course: '+ str(j))
+            continue
+    try:    
+        if data[currentStudent]['class']['H'][' ACAD'] is 'M' or 'A' or 'B' or 'C':
+                data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1.5
+        elif data[currentStudent]['class']['H'][' ACAD'] is 'D':
+                data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+0.5
+        if data[currentStudent]['class']['H']['COOP'] is 'E' or 'S':
+            data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1
         wh =False;
-        if data[currentStudent]['class'][str(j)]['ABS'] is '0' and int(data[currentStudent]['class'][str(j)]['TAR']) < 3:
+        if data[currentStudent]['class']['H']['ABS'] is '0' and int(data[currentStudent]['class']['H']['TAR']) < 3:
             wh=True
-            data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+2
+            data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1
         if wh is True:
-            data[currentStudent]['class'][str(j)]['pts']=data[currentStudent]['class'][str(j)]['pts']+2
-       #HomeRoom Below.
-    if data[currentStudent]['class']['H'][' ACAD'] is 'M' or 'A' or 'B' or 'C':
-            data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1.5
-    elif data[currentStudent]['class']['H'][' ACAD'] is 'D':
-            data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+0.5
-    if data[currentStudent]['class']['H']['COOP'] is 'E' or 'S':
-        data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1
-    wh =False;
-    if data[currentStudent]['class']['H']['ABS'] is '0' and int(data[currentStudent]['class']['H']['TAR']) < 3:
-        wh=True
-        data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1
-    if wh is True:
-        data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1
+            data[currentStudent]['class']['H']['pts']=data[currentStudent]['class']['H']['pts']+1
+    except KeyError:
+        print('Error with '+currentStudent+' could not find course: H or points to add')
+        continue
         #Now Tally individual student totals.
-    for j in range(1,7):
-        data[currentStudent]['Tpts']=data[currentStudent]['Tpts']+data[currentStudent]['class'][str(j)]['pts']
-    data[currentStudent]['Tpts']=data[currentStudent]['Tpts']+data[currentStudent]['class']['H']['pts']
-            
+    try:
+        for j in range(1,7):
+            data[currentStudent]['Tpts']=data[currentStudent]['Tpts']+data[currentStudent]['class'][str(j)]['pts']
+        data[currentStudent]['Tpts']=data[currentStudent]['Tpts']+data[currentStudent]['class']['H']['pts']
+    except KeyError:
+        continue
